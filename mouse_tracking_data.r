8fd5ac8ccb7c25321e7738e11da6a350cfe1051b
Alexandria Baldizon <alexamb097@gmail.com>
2024-04-16 21:20:04 -0500

    created r script for mouse tracking project

---
+++ mouse_tracking_data.r
@@ -0,0 +1,31 @@
+library(readr)
+library(dplyr)
+library(tidyverse)
+library(rstatix)
+library(ggplot2)
+library(mousetrap)
+
+# save raw data from mousetra package
+raw_data <- KH2017_raw
+
+# create mousetrap object using mt_import_mousetrap
+mt_data <- mt_import_mousetrap(
+    raw_data = raw_data,
+    xpos_label = "xpos_",
+    ypos_label = "ypos_",
+    timestamps_label = "timestamps_"
+) %>%
+    # remap the coordinates so that the trajectories go to the right
+    mt_remap_symmetric() %>%
+    # set the start coordinates the same for all trajectories
+    mt_align_start(start = c(0, 0)) %>%
+    # time normalize trajectories to 101 equally sized time steps
+    mt_time_normalize() %>%
+    # only keep the correct trials
+    mt_subset(correct == 1)
+
+# view the structure of the mousetrap object
+str(mt_data)
+
+# mousetrap object now has:
+# data, trajectories, and tn_trajectories (time-normalized)
